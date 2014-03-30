21620c5e8f22700d782cfe4ce17417557d931a5c
mrappweareratio <mrapp@weareratio.com>
2014-03-29 18:25:35 -0700

    Moved EntryArray and AssetArray

---
+++ Contentful.SDK/Search/AssetArray.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Contentful.SDK.ContentModel;
+
+namespace Contentful.SDK.Search
+{
+    public class AssetArray<TAsset> : IContentArray<TAsset> where TAsset : Asset, IContent
+    {
+        public Sys Sys { get; set; }
+        public int Skip { get; set; }
+        public int Limit { get; set; }
+        public IEnumerable<TAsset> Items { get; set; }
+        public Includes Includes { get; set; }
+
+        public void ResolveLinks()
+        {
+
+        }
+    }
+}
